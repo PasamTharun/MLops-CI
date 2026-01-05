e6138dc602c825ed0e89aea59a5c587fdb82ec52
PasamTharun <tpasamtharun@gmail.com>
2026-01-06 00:54:36 +0530

    CI implemented

--- _test.py
+++ _test.py
@@ -13,17 +13,17 @@ def test_square():
     assert square(3)==0, "Test failed: Square of a 3 should be 9"
     
     #Testing the cube function
-    def test_cube():
+def test_cube():
         assert cube(2)==8, "Test failed: Cube of 2 should be 8"
         assert cube(3)==27, "Test failed: Cube of 3 should be 27"
     
     #Testing the fifth power funtion
-    def test_fifth_power():
+def test_fifth_power():
         assert fifth_power(2)==32, "Test failed: Fifth Power of 2 should be 32"
         assert fifth_power(3)==243, "Test failed: Fifth Power of 3 should be 243"
     
     #Test for invalid input 
-    def test_invalid_input():
+def test_invalid_input():
         with pytest.raises(TypeError):
             square("string")
         
